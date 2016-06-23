30ca76733895171b750dadbbcb16e50b023d2a0e
Jonathan Muller <koren@pukogames.com>
2016-06-24 00:49:16 +0200

    Use ENV var for API path, set staging URL in env var in phpunit

--- src/API.php
+++ src/API.php
@@ -2,22 +2,20 @@
 
 namespace DialOnce;
 
+if (empty($_ENV['DIALONCE_API_URL'])) {
+    $_ENV['DIALONCE_API_URL'] = 'https://api.dial-once.com/';
+}
+
 class API {
     /**
-     * The Dial Once API URL
-     * @var string
-     */
-    private static $api_path = 'http://api.dialonce.io/';
-
-    /**
      * Perform a POST request against the Dial Once API
-     * @param  [string] $url    The URL to query
+     * @param  [string] $path    The URL to query
      * @param  [array] $params ["key" => "value"] array to use as parameters
      * @param  [string(opt.)] $token  a token used to auth yourself to the API. See DialOnce\Application class to grab a token
      * @return [string]       The API answer (as string)
      */
-    static function post($url, $params, $token = NULL) {
-        $ch = curl_init(API::$api_path.$url);
+    static function post($path, $params, $token = NULL) {
+        $ch = curl_init($_ENV['DIALONCE_API_URL'].$path);
 
         curl_setopt($ch, CURLOPT_POST, count($params));
         curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($params));
@@ -34,13 +32,13 @@ class API {
 
     /**
      * [get description]
-     * @param  [string] $url    The URL to query
+     * @param  [string] $path    The URL to query
      * @param  [array] $params ["key" => "value"] array to use as parameters
      * @param  [string(opt.)] $token  a token used to auth yourself to the API. See DialOnce\Application class to grab a token
      * @return [string]       The API answer (as string)
      */
-    static function get($url, $params, $token = NULL) {
-        $ch = curl_init(API::$api_path.$url.'/?'.http_build_query($params));
+    static function get($path, $params, $token = NULL) {
+        $ch = curl_init($_ENV['DIALONCE_API_URL'].$path.'/?'.http_build_query($params));
 
         if (!empty($token)) {
             curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: Bearer '.$token));
